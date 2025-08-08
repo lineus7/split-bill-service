848f4221eca4f032cb6d0474416bd0eaabba7fa4
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-08 22:52:54 +0700

    Enhance Migration

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -7,3 +7,5 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE INDEX idx_users_deleted_at ON users (deleted_at) WHERE deleted_at IS NOT NULL;
