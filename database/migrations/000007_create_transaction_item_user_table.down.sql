69054c970fca3deccca320ea1c285404268c0a8f
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-11 16:29:08 +0700

    Add TransactionItemUsers Table

---
+++ database/migrations/000007_create_transaction_item_user_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS transaction_item_users;
---
+++ database/migrations/000007_create_transaction_item_user_table.up.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS transaction_item_users;
+CREATE TABLE transaction_item_users (
+	id SERIAL PRIMARY KEY,
+    transaction_item_id INT NOT NULL REFERENCES transaction_items(id),
+    user_id INT REFERENCES users(id),
+    alternative_customer_name VARCHAR(255)
+);
+
+CREATE INDEX idx_transaction_item_users_transaction_item_id ON transaction_item_users (transaction_item_id);
