9d6418e97c3fac2a4edb2a9129f82081da46f766
Ryan Willyanto <ryanwillyanto@gmail.com>
2025-08-11 20:00:45 +0700

    fix receipt generation

--- internal/services/receipt_service.go
+++ internal/services/receipt_service.go
@@ -1,10 +1,15 @@
 package services
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"mime/multipart"
+	"net/http"
 	"split-bill-service/internal/repositories"
 	"split-bill-service/prompts"
+	"split-bill-service/utils"
+	"strings"
 )
 func NewReceiptService(repos *repositories.RepositoriesSet) *ReceiptService {
 	return &ReceiptService{
@@ -27,5 +32,22 @@ func (s *ReceiptService) GenerateReceiptJSON(fileHeader *multipart.FileHeader) (
 		return "", err
 	}
 	
-	return s.geminiRepository.GenerateContentWithBytes(fileBytes, prompts.GenerateReceiptPrompt)
+	resultString, err := s.geminiRepository.GenerateContentWithBytes(fileBytes, prompts.GenerateReceiptPrompt)
+	if err != nil {
+		return "", err
+	}
+
+	resultString = strings.TrimPrefix(resultString, "```json\n")
+	resultString = strings.TrimSuffix(resultString, "\n```")
+
+	var jsonMap map[string]interface{}
+    if err := json.Unmarshal([]byte(resultString), &jsonMap); err != nil {
+        return "", fmt.Errorf("failed to parse JSON: %w", err)
+    }
+
+    if _, ok := jsonMap["error"]; ok {
+        return "", utils.NewAppError(http.StatusBadRequest, jsonMap["error"].(string), nil)
+    }
+
+	return resultString, nil
 }
